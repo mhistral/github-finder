6d841afea55fd01454ce201151416ed174fd7c77
Kimmy <kimberlyparrenas@gmail.com>
2022-06-27 19:44:39 +0800

    fix navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -19,11 +19,6 @@ function Navbar({ title }) {
 						<Link to="/" className="btn btn-ghost btn-sm rounded-btn">
 							Home
 						</Link>
-					</div>
-				</div>
-
-				<div className="flex-1 px-2 mx-2">
-					<div className="flex justify-end">
 						<Link to="/about" className="btn btn-ghost btn-sm rounded-btn">
 							About
 						</Link>
